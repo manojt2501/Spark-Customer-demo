064494e4c173b527a8756d334ef075bb3bfd48ac
manojt2501 <manojt2501@gmail.com>
2021-11-15 01:10:44 +0530

    segregated test scopes in Unit testing

--- src/test/scala/DataParserTest.scala
+++ src/test/scala/DataParserTest.scala
@@ -5,15 +5,17 @@ import org.apache.spark.sql.functions.{udf}
 class DataParserTest extends AnyFunSuite with SparkSessionTestWrapper with DataFrameTestUltis {
   import spark.implicits._
 
- val sourceDF = Seq(
-  ("cust-1","manoj","house1,stree2,city-9999888888"),
+  val sourceDF = Seq(
+    ("cust-1","manoj","house1,stree2,city-9999888888"),
     ("cust-2","ajay","9989891234"),
     ("cust-3","friend1","house1")
   ).toDF("cust_id","cust_name","cust_contact")
 
-//  sourceDF.show(false)
-  val addAddressAndPhoneUDF = udf(getContactDetails)
-
+  val expectedDF = Seq(
+    ("cust-1","manoj","house1,stree2,city","9999888888"),
+    ("cust-2","ajay","null","9989891234"),
+    ("cust-3","friend1","house1","null")
+  ).toDF("cust_id","cust_name","Address","phone")
 
   def getContactDetails: String => String = (inputContact:String) =>{
     val hasChars: String = ".*[a-zA-Z]+.*"
@@ -25,21 +27,16 @@ class DataParserTest extends AnyFunSuite with SparkSessionTestWrapper with DataF
       case inputContact if inputContact.indexOf("-") < 0 => s"${null};${inputContact}"
     }
   }
+  val addAddressAndPhoneUDF = udf(getContactDetails)
 
   val resDF = processInputRecords(sourceDF,addAddressAndPhoneUDF)
 
-  resDF.show(false)
 
-  val expectedDF = Seq(
-    ("cust-1","manoj","house1,stree2,city","9999888888"),
-    ("cust-2","ajay","null","9989891234"),
-    ("cust-3","friend1","house1","null")
-  ).toDF("cust_id","cust_name","Address","phone")
+  test("Dataframe compare"){
 
-  expectedDF.show(false)
+  assert(asserData(resDF,expectedDF))}
 
-  assert(assertSchema(resDF.schema,expectedDF.schema))
-
-  assert(asserData(resDF,expectedDF))
+  test("DataframeSchema compare"){
+  assert(assertSchema(resDF.schema,expectedDF.schema))}
 
 }
